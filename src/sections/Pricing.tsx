3ed33acfcfaa204725f554787b396d1c2e9baf4a
Sakshi <sakshij4869005@gmail.com>
2026-05-02 18:21:39 +0530

    change in pricing add key

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -64,8 +64,8 @@ export const Pricing = () => {
         </div>
         <p className="section-description mt-5">Free forever. Upgrade for unlimited tasks, better security, and exclusive features.</p>
         <div className='flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center'>
-          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }) => (
-            <div className={twMerge('card', inverse === true && 'border-black bg-black text-white')}>
+          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features}, index) => (
+            <div key={index} className={twMerge('card', inverse === true && 'border-black bg-black text-white')}>
               <div className='flex justify-between'>
                 <h3 className={twMerge('text-lg font-bold text-black/50', inverse === true && 'text-white/60')}>{title}</h3>
                 {popular === true && (
@@ -81,8 +81,8 @@ export const Pricing = () => {
               </div>
               <button className={twMerge('btn btn-primary w-full mt-[30px]', inverse === true && 'bg-white text-black')}>{buttonText}</button>
               <ul className='flex flex-col gap-5 mt-8'>
-                {features.map(features => (
-                  <li className='text-sm flex items-center gap-4'>
+                {features.map((features, i) => (
+                  <li key={i} className='text-sm flex items-center gap-4'>
                     <CheckIcon className='h-6 w-6' />
                     <span>{features}</span>
                   </li>
